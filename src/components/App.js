fd18c8f370d9fd74f7319606fb5247c039b74582
saikeerthi555 <43855677+saikeerthi555@users.noreply.github.com>
2021-01-26 12:34:21 +0530

    map function

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes.js";
 
+function createNotes(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      Content={noteItem.content}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <Header />
-      <Note title="This is the note title" content="This is the note content" />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
